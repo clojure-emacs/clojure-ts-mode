61fc513590ca3ed788a5568623ae419cd3f554f7
Danny Freeman <danny@dfreeman.email>
2023-09-10 13:12:41 -0400

    clojure-ts--ensure-grammars: Add comment about use of dynamic scope

--- clojure-ts-mode.el
+++ clojure-ts-mode.el
@@ -828,6 +828,9 @@ forms like deftype, defrecord, reify, proxy, etc."
 (defun clojure-ts--ensure-grammars ()
   "Install required language grammars if not already available."
   (when clojure-ts-ensure-grammars
+    ;; `treesit-language-source-alist' is dynamically scoped.
+    ;; Setting it here allows `treesit-install-language-gramamr' to pick up
+    ;; the grammar recipes we want without modifying what the user has set.
     (let ((treesit-language-source-alist clojure-ts-grammar-recipes))
       (unless (treesit-language-available-p 'clojure nil)
         (message "Installing clojure tree-sitter grammar.")
