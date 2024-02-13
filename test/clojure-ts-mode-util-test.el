4caa7bb244c36f87df12a51d9f1bd0aa70a2c34f
p4v4n <pavan.mantha99@gmail.com>
2024-02-14 02:33:14 +0530

    Add first unit test with buttercup

---
+++ test/clojure-ts-mode-util-test.el
@@ -0,0 +1,29 @@
+;;; clojure-ts-mode-util-test.el --- Clojure TS Mode: util test suite  -*- lexical-binding: t; -*-
+
+;; Copyright © 2022-2024 Danny Freeman
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; The unit test suite of Clojure TS Mode
+
+(require 'clojure-ts-mode)
+(require 'buttercup)
+
+(describe "clojure-ts-mode-version"
+  (it "should not be nil"
+    (expect clojure-ts-mode-version)))
