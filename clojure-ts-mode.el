ce90a9b6650421252c7e2c7730ff7a7b8d7fe558
Dieter Komendera <dieter@komendera.com>
2024-03-31 23:11:22 +0200

    Don't indent toplevel `def`s with metadata

--- clojure-ts-mode.el
+++ clojure-ts-mode.el
@@ -810,6 +810,13 @@ forms like deftype, defrecord, reify, proxy, etc."
            (clojure-ts--match-fn-docstring parent)
            (clojure-ts--match-method-docstring parent))))
 
+(defun clojure-ts--match-toplevel-with-meta (node parent _bol)
+  "Match NODE when it is toplevel form and it has metadata"
+  (let* ((grandparent (treesit-node-parent parent)))
+    (and (string-equal "source" (treesit-node-type grandparent))
+         (clojure-ts--list-node-p parent)
+         (treesit-node-child-by-field-name parent "meta"))))
+
 (defun clojure-ts--semantic-indent-rules ()
   "Return a list of indentation rules for `treesit-simple-indent-rules'."
   `((clojure
@@ -822,6 +829,7 @@ forms like deftype, defrecord, reify, proxy, etc."
      (clojure-ts--match-threading-macro-arg prev-sibling 0)
      ;; https://guide.clojure.style/#vertically-align-fn-args
      (clojure-ts--match-function-call-arg (nth-sibling 2 nil) 0)
+     (clojure-ts--match-toplevel-with-meta parent 0)
      ;; Literal Sequences
      ((parent-is "list_lit") parent 1) ;; https://guide.clojure.style/#one-space-indent
      ((parent-is "vec_lit") parent 1) ;; https://guide.clojure.style/#bindings-alignment
