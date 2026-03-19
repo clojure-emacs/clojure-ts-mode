45cf2fb59c59b753399cc9ef026c44902f9fc25b
Bozhidar Batsov <bozhidar@batsov.dev>
2026-03-19 11:20:23 +0200

    Add integration test suite
    Verify font-lock and imenu against real sample files to catch
    regressions that unit tests on small snippets might miss.

---
+++ test/clojure-ts-mode-integration-test.el
@@ -0,0 +1,98 @@
+;;; clojure-ts-mode-integration-test.el --- Clojure TS Mode: integration test suite  -*- lexical-binding: t; -*-
+
+;; Copyright © 2025-2026 Bozhidar Batsov
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Integration tests that load sample resource files and verify that
+;; font-lock and imenu work correctly end-to-end.
+
+;;; Code:
+
+(require 'clojure-ts-mode)
+(require 'buttercup)
+(require 'imenu)
+(require 'test-helper "test/test-helper")
+
+(describe "integration: font-lock on sample files"
+  (before-all
+    (unless (treesit-language-available-p 'clojure)
+      (signal 'buttercup-pending "tree-sitter Clojure grammar not available")))
+
+  (it "applies expected font-lock faces to indentation.clj"
+    (let ((file (clojure-ts-test--resource-file "indentation.clj")))
+      (with-temp-buffer
+        (insert-file-contents file)
+        (let ((treesit-font-lock-level 4))
+          (clojure-ts-mode))
+        (font-lock-ensure)
+        ;; Check that "defn" keyword is fontified
+        (goto-char (point-min))
+        (search-forward "(defn f")
+        (expect (get-text-property (1+ (match-beginning 0)) 'face)
+                :to-equal 'font-lock-keyword-face)
+        ;; Check that a docstring is fontified
+        (goto-char (point-min))
+        (search-forward "\"Docstring")
+        (expect (get-text-property (match-beginning 0) 'face)
+                :to-equal 'font-lock-doc-face)
+        ;; Check that a comment is fontified
+        (goto-char (point-min))
+        (search-forward ";; double")
+        (expect (get-text-property (match-beginning 0) 'face)
+                :to-equal 'font-lock-comment-face))))
+
+  (it "applies expected font-lock faces to test.clj"
+    (let ((file (clojure-ts-test--resource-file "test.clj")))
+      (with-temp-buffer
+        (insert-file-contents file)
+        (let ((treesit-font-lock-level 3))
+          (clojure-ts-mode))
+        (font-lock-ensure)
+        ;; Check that "ns" is fontified
+        (goto-char (point-min))
+        (search-forward "(ns ")
+        (expect (get-text-property (1+ (match-beginning 0)) 'face)
+                :to-equal 'font-lock-keyword-face)))))
+
+(describe "integration: imenu on sample files"
+  (before-all
+    (unless (treesit-language-available-p 'clojure)
+      (signal 'buttercup-pending "tree-sitter Clojure grammar not available")))
+
+  (it "builds imenu index from indentation.clj"
+    (let ((file (clojure-ts-test--resource-file "indentation.clj")))
+      (with-temp-buffer
+        (insert-file-contents file)
+        (clojure-ts-mode)
+        (let ((index (imenu--make-index-alist)))
+          ;; Should have Function and Variable categories
+          (expect (assoc "Function" index) :not :to-be nil)
+          (expect (assoc "Variable" index) :not :to-be nil)))))
+
+  (it "builds imenu index from refactoring.clj"
+    (let ((file (clojure-ts-test--resource-file "refactoring.clj")))
+      (with-temp-buffer
+        (insert-file-contents file)
+        (clojure-ts-mode)
+        (let* ((index (imenu--make-index-alist))
+               (flatten-index (imenu--flatten-index-alist index t)))
+          ;; Should find the namespace
+          (expect (assoc "Namespace:refactoring" flatten-index) :not :to-be nil))))))
+
+;;; clojure-ts-mode-integration-test.el ends here
